c53b74d5c173f37c0781ece43e2c15fb59f0ffac
Arjun Khunti <39767903+ArjunKhunti@users.noreply.github.com>
2019-05-15 17:43:47 +0530

    Static Concept Explanation

---
+++ StaticExp1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public static int round = 5;
+    public static string msg = "HelloWorld";
+    // Start is called before the first frame update
+    public void Start()
+    { }
+        
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ StaticExp2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class StaticExp2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Rounds are : " + StaticExp1.round);
+        Debug.Log("Message that " + StaticExp1.msg);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
