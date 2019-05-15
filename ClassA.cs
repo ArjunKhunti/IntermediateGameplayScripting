df301a934c8fc334952505e0379e2efd813497f6
Arjun Khunti <39767903+ArjunKhunti@users.noreply.github.com>
2019-05-15 17:52:33 +0530

    Method Overloading

---
+++ ClassA.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassA : MonoBehaviour
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public string Add(string str1, string str2)
+    {
+        return str1 + str2;
+    }
+}
---
+++ ClassB.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassB : MonoBehaviour
+{
+    ClassA obj= new ClassA();
+
+    obj.Add(44, 66);
+    obj.Add("Hello", "World");
+
+}
