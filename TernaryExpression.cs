e0bd121c4d8ee55f7270f80412e3617a9770e21a
Arjun Khunti <39767903+ArjunKhunti@users.noreply.github.com>
2019-05-15 17:30:09 +0530

    Ternary Expression Example

---
+++ TernaryExpression.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TernaryExpression : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = 5;
+        string msg;
+        msg = num > 3 ? "Greater than 3" : "Less than 3";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
