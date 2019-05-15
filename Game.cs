fb7144587cca6aae20de8b9122c82ce56acc700c
Arjun Khunti <39767903+ArjunKhunti@users.noreply.github.com>
2019-05-15 17:25:05 +0530

    Player and Game File

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player myPlayer = new Player();
+        myPlayer.Exp = 5;
+        int x = myPlayer.Exp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int exp;
+
+    public int Exp
+    {
+        set
+        {
+            exp = value;
+        }
+        get
+        {
+            return exp;
+        }
+    }
+
+    public void Level
+    {
+        get
+        {
+            return exp / 5000;
+        }
+        set
+        {
+            exp = exp * 5000;
+        }
+    }
+
+    public void Health { get; set; }
+}
